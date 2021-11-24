c529a1a0d4e72b03b886e730d366d023eb4c3543
Ubuntu <ubuntu@ip-172-31-40-13.ec2.internal>
2021-11-24 18:24:40 +0000

    Created basic math and area of rectangle programs.

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+# Created by Alexander Matheson
+# Created on November 24 2021
+# This program calculates the area and perimeter of a rectangle
+
+
+def main():
+    # this program calculates area and perimeter
+    print("This rectangle has a length of 10cm and a width of 20cm")
+    print("10cm X 20cm")
+    print("Area={}cm squared".format(10*20))
+    print("")
+    print("2(10cm + 20cm)")
+    print("Perimeter is {} cm".format(2*(10+20)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+# Created by Alexander Matheson
+# Created on November 24 2021
+# This program does basic math
+
+
+def main():
+    # this program does basic math
+    print(9+2)
+    print("7-3={}".format(7-3))
+    print("4/2={}".format(4/2))
+    print("4+4/2={}".format(4+4/2))
+    print("3+4*2={}".format(3+4*2))
+    print("5+2^3={}".format(5+2^3))
+    print("5+2**3={}".format(5+2**3))
+
+
+if __name__ == "__main__":
+    main()
